8da81c67ef1316b22c153a46b41730045162122b
Skythrill256 <anjishnuganguly773@gmail.com>
2024-08-18 17:39:35 +0530

    first commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+// Global meta data
+useHead({
+  link: [
+    {
+      rel: 'icon',
+      href: '',
+    },
+  ],
+});
+</script>
+
+<template>
+  <Html lang="en">
+    <TransitionOverlay />
+    <NuxtPage />
+  </Html>
+</template>
